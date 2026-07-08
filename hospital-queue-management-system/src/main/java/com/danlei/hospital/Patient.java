2ecde493685719281531de7dc83d59e7bc7019d7
Danlei <dli332@aucklanduni.ac.nz>
2026-07-09 01:37:01 +1200

    Add Patient class to the code

---
+++ hospital-queue-management-system/src/main/java/com/danlei/hospital/Patient.java
@@ -0,0 +1,13 @@
+package com.danlei.hospital;
+
+public class Patient {
+    private String name;
+    private int age;
+    private String gender;
+
+    public Patient(String name, int age, String gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+}
