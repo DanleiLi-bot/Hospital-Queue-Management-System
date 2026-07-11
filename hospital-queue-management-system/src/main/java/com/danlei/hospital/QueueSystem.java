a9ab5d0918ee354d8607c805da3547e8e252b710
Danlei <dli332@aucklanduni.ac.nz>
2026-07-11 20:52:28 +1200

    Add mathod declerations in the QueueSystem class

--- hospital-queue-management-system/src/main/java/com/danlei/hospital/QueueSystem.java
+++ hospital-queue-management-system/src/main/java/com/danlei/hospital/QueueSystem.java
@@ -1,9 +1,29 @@
 package com.danlei.hospital;
+import com.danlei.hospital.Main.Gender;
 
 public class QueueSystem {
     public QueueSystem() {
+        loadDoctors();
+    }
+
+    private void loadDoctors() {
+        // Implementation for loading doctors
+    }
+
+    public void addPatient(String name, int age, Gender gender) {
 
     }
 
-    
+    public void showQueue(String patientName) {
+
+    }
+
+    public void searchPatient(String patientName) {
+
+    }
+
+    public void callNextPatient() {
+
+    }
+
 }
